9bae9c6376378e0c9ba1c476b0a5564df6e0ff23
BernardoModerno <bernardo.moderno@gmail.com>
2023-02-25 19:15:09 -0300

    Testando rota de categoria

--- rentx-node/src/server.ts
+++ rentx-node/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Running' });
-});
+app.use(categoriesRoutes);
 
-app.listen(3333, () => console.log('Server is running'));
+app.use(express.json());
+
+app.listen(3333);
