abbf7df5311f9cb083ec57022ead6d89e8740e9a
BernardoModerno <bernardo.moderno@gmail.com>
2023-02-25 19:36:29 -0300

    model categoria

--- rentx-node/src/routes/categories.routes.ts
+++ rentx-node/src/routes/categories.routes.ts
@@ -1,14 +1,29 @@
 import { Router } from 'express';
+import { Category } from '../models/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    categories.push({ name, description });
-    response.sendStatus(201);
+    const category = new Category();
+    Object.assign(category, {
+        name,
+        description,
+        created_at: new Date(),
+    });
+
+    categories.push(category);
+    return response.sendStatus(201);
+});
+
+categoriesRoutes.get('/', (request, response) => {
+    // const categoriesWithoutId = categories.map((categories) => {
+    //   return { ...categories, id: undefined };
+    // });
+    return response.status(200).json(categories);
 });
 
 export { categoriesRoutes };
